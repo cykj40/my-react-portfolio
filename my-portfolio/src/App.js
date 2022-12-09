c1c7b94f7c97a43f63ec47914d195c207aa20f81
cykj40 <102045473+cykj40@users.noreply.github.com>
2022-12-09 12:09:36 -0500

    app.js just getting started

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Landing from "./pages/Landing";
+import Navbar from "./components/Navbar";
+import AboutMe from "./pages/AboutMe";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+
+import projects from "./assets/projects.js";
+import skills from "./assets/skills.js";
+import language from "./assets/skills-language.js";
+import framework from "./assets/skills-framework.js";
+import database from "./assets/skills-database.js";
+import tool from "./assets/skills-tools.js";
 
 function App() {
+  const [page, setPage] = useState("About Me");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="scroll">
+      <Landing setPage={setPage} />
+      <Navbar page={page} setPage={setPage} />
+      <AboutMe setPage={setPage} /> 
+      <Projects setPage={setPage} projects={projects} />
+      <Resume
+        setPage={setPage}
+        skills={skills}
+        language={language}
+        framework={framework}
+        database={database}
+        tool={tool}
+      />
+      <Contact setPage={setPage} />
+    </main>
   );
 }
 
